32745b674922b4544f1293bb0c94173525208ff6
TOMI <131228420+Omni-hub111@users.noreply.github.com>
2024-06-05 20:59:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -117,9 +117,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 photoRow.appendChild(img);
             });
 
-            // 最後の写真の次に最初の写真を追加
-            const lastPhotoClone = photoRow.lastElementChild.cloneNode(true);
-            photoRow.insertBefore(lastPhotoClone, photoRow.firstElementChild);
+  
         })
         .catch(error => console.error("Error fetching photos:", error));
 });
@@ -204,94 +202,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-window.addEventListener('DOMContentLoaded', function() {
-    const accessTitle = document.querySelector('.Access-Title');
-    const frameImage = document.querySelector('.rectangle-container');
-
-    function updateFrameImagePosition() {
-        const accessTitleWidth = accessTitle.offsetWidth;
-        const screenWidth = window.innerWidth;
-        const additionalMargin = (screenWidth <= 800) ? 20 : 10; // Increase margin if screen width is less than or equal to 800px
-        frameImage.style.top = `${accessTitleWidth + additionalMargin}px`;
-    }
-
-    // Initial position update
-    updateFrameImagePosition();
-
-    // Update position on window resize
-    window.addEventListener('resize', updateFrameImagePosition);
-});
-
-window.onload = function() {
-    var rectangle = document.getElementById("rectangle");
-
-    if (window.innerHeight > 400 && window.innerWidth > 400) {
-        window.addEventListener("scroll", function() {
-            // ページのスクロール位置を取得
-            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-            // 開始位置と終了位置を設定
-            var startPosition = 400; // 開始位置（スクロールトップがこの位置に達するとRectangleが現れます）
-            var endPosition = 4000; // 終了位置（スクロールトップがこの位置に達するとRectangleが消えます）
-
-            // スクロール位置が開始位置と終了位置の間にある場合、Rectangleの高さを設定
-            if (scrollTop >= startPosition && scrollTop <= endPosition) {
-                // スクロール位置に応じてRectangleの高さを計算
-                var scrollPercentage = (scrollTop - startPosition) / (endPosition - startPosition);
-                var rectangleHeight = scrollPercentage * 120 + "%"; // 120%の高さを設定
-                rectangle.style.height = rectangleHeight;
-            } else {
-                // スクロール位置が範囲外の場合、Rectangleを非表示にする
-                rectangle.style.height = "0";
-            }
-        });
-    }
-};
-
-document.addEventListener('scroll', function() {
-    const elements = document.querySelectorAll('.Events');
-    const windowHeight = window.innerHeight;
-    const scrollPosition = window.scrollY + windowHeight;
-
-    elements.forEach(function(element) {
-        const elementPosition = element.getBoundingClientRect().top + window.scrollY;
-        if (scrollPosition > elementPosition + element.clientHeight / 2) {
-            element.classList.add('show');
-        } else {
-            element.classList.remove('show');
-        }
-    });
-});
-
-document.addEventListener('scroll', function() {
-    const elements = document.querySelectorAll('.Images');
-    const windowHeight = window.innerHeight;
-    const scrollPosition = window.scrollY + windowHeight;
-
-    elements.forEach(function(element) {
-        const elementPosition = element.getBoundingClientRect().top + window.scrollY;
-        if (scrollPosition > elementPosition + element.clientHeight / 2) {
-            element.classList.add('show2');
-        } else {
-            element.classList.remove('show2');
-        }
-    });
-});
-
-window.addEventListener('DOMContentLoaded', function() {
-    const accessTitle = document.querySelector('.Event-Title');
-    const frameImage = document.querySelector('.youtube-container');
-
-    function updateFrameImagePosition() {
-        const accessTitleWidth = accessTitle.offsetWidth;
-        const screenWidth = window.innerWidth;
-        const additionalMargin = (screenWidth <= 800) ? 20 : 10; // Increase margin if screen width is less than or equal to 800px
-        frameImage.style.top = `${accessTitleWidth + additionalMargin}px`;
-    }
-
-    // Initial position update
-    updateFrameImagePosition();
-
-    // Update position on window resize
-    window.addEventListener('resize', updateFrameImagePosition);
-});
